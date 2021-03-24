3aaaf15121c226ec8faa91962f40d7d61c5f4952
Isaac Mills <rooster0055@protonamil.com>
2021-03-24 00:30:14 -0400

    Add electron-compatibility with electron-env-builder

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
 declare module "globkey" {
-    export function on(callback: (action: string, key: string) => void): void;
-    export function raw(callback: (keypair: [string, string]) => void): void;
+    export function raw(callback: (keypair: string[]) => void): void;
     export function capturecombo(): [string, string];
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,6 @@
-use device_query::{DeviceQuery, DeviceState, Keycode};
+use device_query::{DeviceQuery, DeviceState};
 use node_bindgen::derive::node_bindgen;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
-
-struct Changed {
-    key: Keycode,
-    op: String,
-}
-
-#[node_bindgen]
-async fn on<F: Fn(String, String)>(returnjs: F) {
-    let device_state = DeviceState::new();
-    let mut prev_keys = vec![];
-    loop {
-        let keys = device_state.get_keys();
-        if keys != prev_keys {
-            let mut changed: Vec<Changed> = Vec::new();
-            keys.clone().into_iter().for_each(|i| {
-                if !prev_keys.contains(&i) {
-                    changed.push(Changed {
-                        key: i,
-                        op: "keydown".to_string(),
-                    });
-                }
-            });
-            prev_keys.clone().into_iter().for_each(|i| {
-                if !keys.contains(&i) {
-                    changed.push(Changed {
-                        key: i,
-                        op: "keyup".to_string(),
-                    });
-                }
-            });
-            changed.into_iter().for_each(|i| {
-                returnjs(i.op, format!("{}", i.key));
-            });
-        }
-        prev_keys = keys;
-    }
-}
 
 #[node_bindgen]
 async fn raw<F: Fn(Vec<String>)>(returnjs: F) {
