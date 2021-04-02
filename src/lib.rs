cf841f8fb25c0cf5cf00eee55e38632faeadbeaf
Isaac Mills <rooster0055@protonmail.com>
2021-04-02 10:53:26 -0400

    Try to fix macos 3rd attempt

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,28 @@
+use std::sync::RwLock;
+
 use device_query::{DeviceQuery, DeviceState};
+use lazy_static::lazy_static;
 use node_bindgen::derive::node_bindgen;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+lazy_static! {
+    static ref SHOULDSTOP: RwLock<bool> = RwLock::new(false);
+}
 
 #[node_bindgen]
-async fn raw<F: Fn(Vec<String>)>(returnjs: F) {
+async fn raw<F: Fn(Vec<String>)>(returnjs: F) -> bool {
     let device_state = DeviceState::new();
     let mut prev_keys = vec![];
     loop {
         let keys = device_state.get_keys();
-        if keys != prev_keys {
-            let returnkeys: Vec<String> =
-                keys.clone().into_iter().map(|x| format!("{}", x)).collect();
+        if *SHOULDSTOP.read().unwrap() {
+            return true;
+        } else if keys != prev_keys {
+            let returnkeys: Vec<String> = keys
+                .clone()
+                .into_par_iter()
+                .map(|x| format!("{}", x))
+                .collect();
             returnjs(returnkeys);
         }
         prev_keys = keys;
@@ -17,16 +30,11 @@ async fn raw<F: Fn(Vec<String>)>(returnjs: F) {
 }
 
 #[node_bindgen]
-fn capturecombo() -> Vec<String> {
-    let device_state = DeviceState::new();
-    let mut prev_keys = vec![];
-    loop {
-        let keys = device_state.get_keys();
-        if keys != prev_keys && keys.len() == 2 {
-            let returnkeys: Vec<String> =
-                keys.clone().into_iter().map(|x| format!("{}", x)).collect();
-            return returnkeys;
-        }
-        prev_keys = keys;
-    }
+fn unload() {
+    *SHOULDSTOP.write().unwrap() = true;
+}
+
+#[node_bindgen]
+fn stop() {
+    std::process::exit(0);
 }
