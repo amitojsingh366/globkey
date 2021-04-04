524f8b3c78260415cb1d737e8f32b1d175ce739c
Isaac Mills <rooster0055@protonmail.com>
2021-04-03 23:40:33 -0400

    Add back functions for compatibility

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
 declare module "globkey" {
-    export function raw(): string[];
+    export function raw(callback: (keypair: string[]) => void): Promise<boolean>;
+    export function unload(): void;
+    export function get_keys(): string[];
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,38 @@
+use std::sync::RwLock;
+
 use device_query::{DeviceQuery, DeviceState};
+use lazy_static::lazy_static;
 use node_bindgen::derive::node_bindgen;
 
+lazy_static! {
+    static ref SHOULDSTOP: RwLock<bool> = RwLock::new(false);
+}
+
 #[node_bindgen]
-fn raw() -> Vec<String> {
+async fn raw<F: Fn(Vec<String>)>(returnjs: F) -> bool {
+    let device_state = DeviceState::new();
+    let mut prev_keys = vec![];
+    loop {
+        let keys = device_state.get_keys();
+        if *SHOULDSTOP.read().unwrap() {
+            return true;
+        } else if keys != prev_keys {
+            let returnkeys: Vec<String> =
+                keys.clone().into_iter().map(|x| format!("{}", x)).collect();
+            returnjs(returnkeys);
+        }
+        prev_keys = keys;
+    }
+}
+
+#[node_bindgen]
+fn get_keys() -> Vec<String> {
     let device_state = DeviceState::new();
     let keys = device_state.get_keys();
     keys.clone().into_iter().map(|x| format!("{}", x)).collect()
+}
+
+#[node_bindgen]
+fn unload() {
+    *SHOULDSTOP.write().unwrap() = true;
 }
