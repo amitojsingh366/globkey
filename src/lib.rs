e1a3a57c12976d3452b90f2fbfa7a3cb5bf329db
Isaac Mills <rooster0055@protonmail.com>
2021-04-04 20:26:46 -0400

    remove uneeded static

--- src/lib.rs
+++ src/lib.rs
@@ -13,30 +13,26 @@ static DEVICEMPSC: sync::Lazy<(
     (Mutex::new(tx), Mutex::new(rx))
 });
 
-static DEVICETHREAD: sync::OnceCell<std::thread::JoinHandle<bool>> = sync::OnceCell::new();
-
 static SHOULDSTOP: sync::Lazy<RwLock<bool>> = sync::Lazy::new(|| RwLock::new(false));
 
 #[node_bindgen]
 fn start() {
-    DEVICETHREAD
-        .set(std::thread::spawn(move || loop {
-            let sender = DEVICEMPSC.0.lock();
-            let device_state = DeviceState::new();
-            let mut prev_keys = vec![];
-            loop {
-                let keys = device_state.get_keys();
-                if *SHOULDSTOP.read() {
-                    return true;
-                } else if keys != prev_keys {
-                    let returnkeys: Vec<String> =
-                        keys.clone().into_iter().map(|x| format!("{}", x)).collect();
-                    sender.send(returnkeys).unwrap();
-                }
-                prev_keys = keys;
+    std::thread::spawn(move || loop {
+        let sender = DEVICEMPSC.0.lock();
+        let device_state = DeviceState::new();
+        let mut prev_keys = vec![];
+        loop {
+            let keys = device_state.get_keys();
+            if *SHOULDSTOP.read() {
+                return true;
+            } else if keys != prev_keys {
+                let returnkeys: Vec<String> =
+                    keys.clone().into_iter().map(|x| format!("{}", x)).collect();
+                sender.send(returnkeys).unwrap();
             }
-        }))
-        .unwrap();
+            prev_keys = keys;
+        }
+    });
 }
 
 #[node_bindgen]
