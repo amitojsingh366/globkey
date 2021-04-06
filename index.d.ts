eb5ba5d040defb553022eaf634326164e43c5a39
amitojsingh366 <amitojsingh366@gmail.com>
2021-04-06 19:21:48 +0300

    add description to typings

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,15 @@
 declare module "globkey" {
+    /**
+     * Loads and starts the globkey library
+     */
     export function start(): void;
+    /**
+     * Unloads and stops the globkey library
+     */
     export function unload(): void;
+    /**
+     * @returns an array of the currently pressed keys.
+     * @remarks this method will only fucntion properly if the `start()` function has already been called
+     */
     export function getKeys(): string[];
 }
